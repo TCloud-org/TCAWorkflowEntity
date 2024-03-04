ff9259e3b33724f01d4c3529687a50bea846f997
Tung Dinh <tungxd301@gmail.com>
2024-03-04 09:25:13 -0800

    Changes

---
+++ src/main/java/com/tcloud/tcaworkflowentity/TcaworkflowentityApplication.java
@@ -0,0 +1,13 @@
+package com.tcloud.tcaworkflowentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcaworkflowentityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcaworkflowentityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcloud/tcaworkflowentity/TcaworkflowentityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcloud.tcaworkflowentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcaworkflowentityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
